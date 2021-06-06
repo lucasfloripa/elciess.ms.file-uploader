ed43df0047ea0670c1a6ef2902a0f2b805ad2084
Lucas Gonçalves <lucas.floripa@icloud.com>
2021-06-06 15:33:49 -0300

    test: ensure FileUploadController calls uploadFile with correct values

--- tests/presentation/controllers/file-upload-controller.spec.ts
+++ tests/presentation/controllers/file-upload-controller.spec.ts
@@ -1,7 +1,17 @@
+import { UploadFile } from '@/domain/usecases'
 import { FileUploadController } from '@/presentation/controllers/file-upload-controller'
 import { badRequest } from '@/presentation/helpers'
 import { Validation } from '@/presentation/protocols'
 import { mockValidationStub } from '@/tests/presentation/mocks'
+
+const mockUploadFileStub = (): UploadFile => {
+  class UploadFileStub implements UploadFile {
+    async upload (uploadFileParams: UploadFile.Params): Promise<boolean> {
+      return Promise.resolve(true)
+    }
+  }
+  return new UploadFileStub()
+}
 
 const mockRequest = (): FileUploadController.Request => ({
   fieldname: 'fieldname',
@@ -17,26 +27,35 @@ const mockRequest = (): FileUploadController.Request => ({
 type SutTypes = {
   sut: FileUploadController
   validationStub: Validation
+  uploadFileStub: UploadFile
 }
 
 const makeSut = (): SutTypes => {
   const validationStub = mockValidationStub()
-  const sut = new FileUploadController(validationStub)
-  return { sut, validationStub }
+  const uploadFileStub = mockUploadFileStub()
+  const sut = new FileUploadController(validationStub, uploadFileStub)
+  return { sut, validationStub, uploadFileStub }
 }
 
 describe('FileUpload Controller', () => {
-  test('Should call Validation with correct values', async () => {
+  test('Should call validation with correct values', async () => {
     const { sut, validationStub } = makeSut()
     const validateSpy = jest.spyOn(validationStub, 'validate')
     await sut.handle(mockRequest())
     expect(validateSpy).toHaveBeenCalledWith(mockRequest())
   })
 
-  test('Should return 400 if Validation returns an error', async () => {
+  test('Should return 400 if validation returns an error', async () => {
     const { sut, validationStub } = makeSut()
     jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new Error())
     const httpResponse = await sut.handle(mockRequest())
     expect(httpResponse).toEqual(badRequest(new Error()))
+  })
+
+  test('Should call uploadFile with correct values', async () => {
+    const { sut, uploadFileStub } = makeSut()
+    const validateSpy = jest.spyOn(uploadFileStub, 'upload')
+    await sut.handle(mockRequest())
+    expect(validateSpy).toHaveBeenCalledWith(mockRequest())
   })
 })
