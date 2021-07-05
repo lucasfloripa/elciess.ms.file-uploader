bcf0fbfb49793bc1f2dc050935b74ae64f72af55
Lucas Gonçalves <lucas.floripa@icloud.com>
2021-07-04 21:04:37 -0300

    fix: gitignore and import paths

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,9 +1,9 @@
 /* eslint-disable import/first */
 import 'module-alias/register'
 import dotenv from 'dotenv'
-dotenv.config({ path: './src/main/config/config.env' })
+dotenv.config({ path: './src/main/config/.env' })
 
 import env from '@/main/config/env'
-import { app } from '@/main/config'
+import app from './config/app'
 
 app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`))
