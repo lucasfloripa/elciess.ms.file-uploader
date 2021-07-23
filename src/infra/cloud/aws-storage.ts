c09f835f4f65bb584b4503e983e4a7f107f0d9de
Lucas Gonçalves <lucas.goncalves@calindra.com.br>
2021-07-23 16:08:23 -0300

    feat: ensure AwsStorage uploadFile() returns true when succeeds

--- src/infra/cloud/aws-storage.ts
+++ src/infra/cloud/aws-storage.ts
@@ -1,7 +1,26 @@
 import { UploadFileStorage } from '@/data/protocols'
+import { AwsHelper } from '@/infra/cloud'
+
+import fs from 'fs'
 
 export class AwsStorage implements UploadFileStorage {
   async uploadFile (uploadFileParams: UploadFileStorage.Params): Promise<boolean> {
-    return true
+    const { originalname, path, mimetype } = uploadFileParams
+
+    const awsS3 = AwsHelper.getClientS3()
+
+    const fileContent = await fs.promises.readFile(path)
+
+    const isValid = await awsS3.putObject({
+      Bucket: 'elciess',
+      Key: originalname,
+      ACL: 'public-read',
+      Body: fileContent,
+      ContentType: mimetype
+    }).promise()
+      .then(data => data.ETag)
+
+    // eslint-disable-next-line no-unneeded-ternary
+    return isValid ? true : false
   }
 }
