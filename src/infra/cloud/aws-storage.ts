43b27acb2972eea81e35bba387270da25647ccf6
Lucas Gonçalves <lucas.goncalves@calindra.com.br>
2021-07-28 17:10:54 -0300

    feat: ensure AwsStorage loads a file on success

--- src/infra/cloud/aws-storage.ts
+++ src/infra/cloud/aws-storage.ts
@@ -1,9 +1,9 @@
-import { UploadFileStorage } from '@/data/protocols'
+import { UploadFileStorage, LoadFileStorage } from '@/data/protocols'
 import { AwsHelper } from '@/infra/cloud'
 
 import fs from 'fs'
 
-export class AwsStorage implements UploadFileStorage {
+export class AwsStorage implements UploadFileStorage, LoadFileStorage {
   async uploadFile (uploadFileParams: UploadFileStorage.Params): Promise<boolean> {
     const { bucket, originalname, path, mimetype } = uploadFileParams
     const awsS3 = AwsHelper.getClientS3()
@@ -19,5 +19,17 @@ export class AwsStorage implements UploadFileStorage {
 
     // eslint-disable-next-line no-unneeded-ternary
     return isValid ? true : false
+  }
+
+  async loadFile (loadFileParams: LoadFileStorage.Params): Promise<any> {
+    const { bucket, fileName } = loadFileParams
+    const awsS3 = AwsHelper.getClientS3()
+    const file = awsS3.getObject({
+      Bucket: bucket,
+      Key: fileName
+    }, (_err, data) => {
+      return data
+    })
+    return file
   }
 }
