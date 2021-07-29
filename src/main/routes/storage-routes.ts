97a3443356ed4e3b3f9f121d66d058c648857c87
Lucas Gonçalves <lucas.goncalves@calindra.com.br>
2021-07-29 14:53:44 -0300

    feat: add LoadFileController to storage-routes

--- src/main/routes/storage-routes.ts
+++ src/main/routes/storage-routes.ts
@@ -1,9 +1,10 @@
 import { multerUpload } from '../../main/config/multer'
 import { expressRouterAdapter } from '@/main/adapters'
-import { makeFileUploadController } from '@/main/factories/controllers'
+import { makeFileUploadController, makeFileLoadController } from '@/main/factories/controllers'
 
 import { Router } from 'express'
 
 export default (router: Router): void => {
   router.post('/upload', multerUpload, expressRouterAdapter(makeFileUploadController()))
+  router.get('/load', expressRouterAdapter(makeFileLoadController()))
 }
