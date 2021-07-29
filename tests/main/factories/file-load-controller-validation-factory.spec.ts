e1039c1951edd6d4fa19fa386c1b3d6035202eb8
Lucas Gonçalves <lucas.goncalves@calindra.com.br>
2021-07-29 14:41:15 -0300

    test: ensure fileLoadController Validation works correctly

---
+++ tests/main/factories/file-load-controller-validation-factory.spec.ts
@@ -0,0 +1,16 @@
+import { Validation } from '@/presentation/protocols'
+import { ValidationComposite, RequiredFieldValidation } from '@/utils/validators'
+import { makeFileLoadValidation } from '@/main/factories/validation'
+
+jest.mock('@/utils/validators/validation-composite')
+
+describe('FileLoadController Validation', () => {
+  test('Should call Validation Composite with all Validators', () => {
+    makeFileLoadValidation()
+    const validations: Validation[] = []
+    for (const field of ['bucket', 'fileName']) {
+      validations.push(new RequiredFieldValidation(field))
+    }
+    expect(ValidationComposite).toHaveBeenCalledWith(validations)
+  })
+})
