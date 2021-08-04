58dd43a0ec523e128909e82b9020ea9323b13e86
Lucas Gonçalves <lucas.goncalves@calindra.com.br>
2021-08-04 15:46:09 -0300

    feat: ensure CloudFileUpload calls idGenerator correctly

--- src/data/usecases/cloud-file-upload.ts
+++ src/data/usecases/cloud-file-upload.ts
@@ -1,12 +1,14 @@
 import { UploadFile } from '@/domain/usecases'
-import { UploadFileStorage } from '@/data/protocols/upload-file-storage'
+import { UploadFileStorage, IdGenerator } from '@/data/protocols'
 
 export class CloudFileUpload implements UploadFile {
   constructor (
-    private readonly uploadFileStorage: UploadFileStorage
-  ) {}
+    private readonly uploadFileStorage: UploadFileStorage,
+    private readonly idGenerator: IdGenerator
+  ) { }
 
   async upload (uploadFileParams: UploadFile.Params): Promise<boolean> {
+    await this.idGenerator.generate()
     const isValid = await this.uploadFileStorage.uploadFile(uploadFileParams)
     return isValid
   }
