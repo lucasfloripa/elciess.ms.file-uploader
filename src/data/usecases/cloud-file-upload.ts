7cfa8a94c4a696898881b60f1b2b043ac1703502
Lucas Gonçalves <lucas.goncalves@calindra.com.br>
2021-08-04 16:23:39 -0300

    feat: ensure CloudFileUpload calls registerFileRepository with correct values

--- src/data/usecases/cloud-file-upload.ts
+++ src/data/usecases/cloud-file-upload.ts
@@ -1,15 +1,26 @@
 import { UploadFile } from '@/domain/usecases'
-import { UploadFileStorage, IdGenerator } from '@/data/protocols'
+import { UploadFileStorage, IdGenerator, RegisterFileRepository } from '@/data/protocols'
 
 export class CloudFileUpload implements UploadFile {
   constructor (
     private readonly uploadFileStorage: UploadFileStorage,
-    private readonly idGenerator: IdGenerator
+    private readonly idGenerator: IdGenerator,
+    private readonly registerFileRepository: RegisterFileRepository
   ) { }
 
   async upload (uploadFileParams: UploadFile.Params): Promise<boolean> {
-    await this.idGenerator.generate()
-    const isValid = await this.uploadFileStorage.uploadFile(uploadFileParams)
+    const { bucket, mimetype, originalname } = uploadFileParams
+    const uuid = await this.idGenerator.generate()
+    let isValid: boolean = false
+    isValid = await this.uploadFileStorage.uploadFile(uploadFileParams)
+    if (isValid) {
+      await this.registerFileRepository.register({
+        id: uuid,
+        bucket,
+        mimetype,
+        originalname
+      })
+    }
     return isValid
   }
 }
