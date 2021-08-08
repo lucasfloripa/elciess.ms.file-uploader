dda995efaa3432eec34efffecdf27525fdcc1ab6
Lucas Gonçalves <lucas.goncalves@calindra.com.br>
2021-08-08 19:47:05 -0300

    feat: getClientDynamo()

--- src/infra/helpers/aws-helper.ts
+++ src/infra/helpers/aws-helper.ts
@@ -2,11 +2,19 @@ import aws from 'aws-sdk'
 
 export const AwsHelper = {
   clientS3: null as aws.S3,
+  clientDynamo: null as aws.DynamoDB.DocumentClient,
 
   getClientS3 (): aws.S3 {
     if (!this.clientS3) {
       this.clientS3 = new aws.S3()
     }
     return this.clientS3
+  },
+
+  getClientDynamo (): aws.DynamoDB.DocumentClient {
+    if (!this.clientDynamo) {
+      this.clientDynamo = new aws.DynamoDB.DocumentClient()
+    }
+    return this.clientDynamo
   }
 }
