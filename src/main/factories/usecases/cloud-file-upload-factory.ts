749d612fc8e88f66e19aca68fe8009da7db42509
Lucas Gonçalves <lucas.goncalves@calindra.com.br>
2021-08-09 14:10:07 -0300

    refactor: update cloudFileUploadFactory dependencies

--- src/main/factories/usecases/cloud-file-upload-factory.ts
+++ src/main/factories/usecases/cloud-file-upload-factory.ts
@@ -1,9 +1,11 @@
 import { CloudFileUpload } from '@/data/usecases'
 import { S3FileStorage } from '@/infra/cloud/s3'
 import { UuidAdapter } from '@/infra/generators'
+import { FileDynamoRepository } from '@/infra/db/dynamodb'
 
 export const makeCloudFileUpload = (): CloudFileUpload => {
   const s3FileStorage = new S3FileStorage()
   const uuidAdapter = new UuidAdapter()
-  return new CloudFileUpload(s3FileStorage, uuidAdapter)
+  const fileDynamoRepository = new FileDynamoRepository()
+  return new CloudFileUpload(s3FileStorage, uuidAdapter, fileDynamoRepository)
 }
