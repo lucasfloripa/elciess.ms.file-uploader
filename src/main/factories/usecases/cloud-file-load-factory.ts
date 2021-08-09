87fde0f995fe180dfab19c7e89d627e2cbdc72f3
Lucas Gonçalves <lucas.goncalves@calindra.com.br>
2021-08-09 17:32:36 -0300

    refactor: update CloudFileLoadFactory with new dependencie

--- src/main/factories/usecases/cloud-file-load-factory.ts
+++ src/main/factories/usecases/cloud-file-load-factory.ts
@@ -1,7 +1,9 @@
 import { CloudFileLoad } from '@/data/usecases'
 import { S3FileStorage } from '@/infra/cloud/s3'
+import { FileDynamoRepository } from '@/infra/db/dynamodb'
 
 export const makeCloudFileLoad = (): CloudFileLoad => {
+  const fileDynamoRepository = new FileDynamoRepository()
   const s3FileStorage = new S3FileStorage()
-  return new CloudFileLoad(s3FileStorage)
+  return new CloudFileLoad(s3FileStorage, fileDynamoRepository)
 }
