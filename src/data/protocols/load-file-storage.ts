f437aab9c317ad32f8327a6cbdb9c7eeeedb1d37
Lucas Gonçalves <lucas.goncalves@calindra.com.br>
2021-08-09 15:43:51 -0300

    refactor: change loadFileStorage params

--- src/data/protocols/load-file-storage.ts
+++ src/data/protocols/load-file-storage.ts
@@ -1,9 +1,10 @@
-import { LoadFile } from '@/domain/usecases'
-
 export interface LoadFileStorage {
   loadFile: (loadFileParams: LoadFileStorage.Params) => Promise<any>
 }
 
 export namespace LoadFileStorage {
-  export type Params = LoadFile.Params
+  export type Params = {
+    originalName: string
+    bucket: string
+  }
 }
