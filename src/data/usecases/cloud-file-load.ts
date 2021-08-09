1037fdf23c9ff82105e30d85d7a95d5af1de02e6
Lucas Gonçalves <lucas.goncalves@calindra.com.br>
2021-08-09 15:36:48 -0300

    feat: ensure CloudFileLoad calls fileFileRepository with correct values

--- src/data/usecases/cloud-file-load.ts
+++ src/data/usecases/cloud-file-load.ts
@@ -1,12 +1,14 @@
 import { LoadFile } from '@/domain/usecases'
-import { LoadFileStorage } from '@/data/protocols'
+import { LoadFileRepository, LoadFileStorage } from '@/data/protocols'
 
 export class CloudFileLoad implements LoadFile {
   constructor (
-    private readonly loadFileStorage: LoadFileStorage
+    private readonly loadFileStorage: LoadFileStorage,
+    private readonly loadFileRepository: LoadFileRepository
   ) {}
 
   async load (params: LoadFile.Params): Promise<any> {
+    await this.loadFileRepository.loadRegister(params)
     const file = await this.loadFileStorage.loadFile(params)
     if (!file) {
       return null
