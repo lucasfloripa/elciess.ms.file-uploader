11afd258e750db1dc515a7c8e5ae5938b584b796
Lucas Gonçalves <lucas.goncalves@calindra.com.br>
2021-08-11 08:50:05 -0300

    feat: presentation middleware AuthMiddleware

---
+++ src/presentation/middlewares/auth-middleware.ts
@@ -0,0 +1,32 @@
+import { HttpResponse, Middleware } from '@/presentation/protocols'
+import { forbidden, ok, serverError, unauthorized } from '@/presentation/helpers'
+import { AccessDeniedError } from '@/presentation/errors'
+import { TokenAuthentication } from '@/domain/usecases'
+
+export class AuthMiddleware implements Middleware {
+  constructor (
+    private readonly roleAuthentication: TokenAuthentication
+  ) { }
+
+  async handle (request: AuthRoleMiddleware.Request): Promise<HttpResponse> {
+    try {
+      const { accessToken } = request
+      if (accessToken) {
+        const isValid = await this.roleAuthentication.auth(accessToken)
+        if (isValid) {
+          return ok(isValid)
+        }
+        return forbidden(new AccessDeniedError())
+      }
+      return unauthorized()
+    } catch (error) {
+      return serverError(error)
+    }
+  }
+}
+
+export namespace AuthRoleMiddleware {
+  export type Request = {
+    accessToken: string
+  }
+}
