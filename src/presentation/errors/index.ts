2861378675e02d6e551263f6cb857ff95005c32a
Lucas Gonçalves <lucas.goncalves@calindra.com.br>
2021-08-11 10:30:41 -0300

    refactor: remove unancessary code

--- src/presentation/errors/index.ts
+++ src/presentation/errors/index.ts
@@ -1,7 +1,5 @@
-export * from './email-in-use-error'
 export * from './server-error'
 export * from './missing-param-error'
-export * from './invalid-param-error'
 export * from './unauthorized-error'
 export * from './access-denied-error'
 export * from './not-found-error'
