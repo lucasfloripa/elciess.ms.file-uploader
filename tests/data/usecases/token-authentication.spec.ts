d6cce8f4b13537c346dd867ea7d0b81d9097ba8b
Lucas Gonçalves <lucas.goncalves@calindra.com.br>
2021-08-11 11:10:03 -0300

    test: ensure TokenAuthentication calls decrypt with correct value

---
+++ tests/data/usecases/token-authentication.spec.ts
@@ -0,0 +1,30 @@
+import { TokenAuthentication } from '@/data/usecases'
+import { Decrypter } from '@/data/protocols'
+
+const mockDescrypterStub = (): Decrypter => {
+  class DecrypterStub implements Decrypter {
+    async decrypt (text: string): Promise<string> {
+      return Promise.resolve('any_data')
+    }
+  }
+  return new DecrypterStub()
+}
+
+export type SutTypes = {
+  sut: TokenAuthentication
+  decrypterStub: Decrypter
+}
+
+const makeSut = (): SutTypes => {
+  const decrypterStub = mockDescrypterStub()
+  const sut = new TokenAuthentication(decrypterStub)
+  return { sut, decrypterStub }
+}
+describe('TokenAuthentication', () => {
+  test('Should call decrypter with correct value', async () => {
+    const { sut, decrypterStub } = makeSut()
+    const decryptSpy = jest.spyOn(decrypterStub, 'decrypt')
+    await sut.auth('any_data')
+    expect(decryptSpy).toBeCalledWith('any_data')
+  })
+})
