a345c99297ddf8e1d65eda0768d16493b679dcda
Lucas Gonçalves <lucas.goncalves@calindra.com.br>
2021-08-13 14:30:24 -0300

    feat: add authMiddleware to storage-routes

--- src/main/factories/middlewares/auth-middleware-factory.ts
+++ src/main/factories/middlewares/auth-middleware-factory.ts
@@ -1,6 +1,6 @@
 import { AuthMiddleware } from '@/presentation/middlewares'
 import { makeTokenAuthentication } from '@/main/factories/usecases'
 
-export const makeAuthAdminMiddleware = (): AuthMiddleware => {
+export const makeAuthMiddleware = (): AuthMiddleware => {
   return new AuthMiddleware(makeTokenAuthentication())
 }
--- src/main/routes/storage-routes.ts
+++ src/main/routes/storage-routes.ts
@@ -1,10 +1,11 @@
 import { multerUpload } from '../../main/config/multer'
-import { expressRouterAdapter } from '@/main/adapters'
+import { expressRouterAdapter, expressMiddlewareAdapter } from '@/main/adapters'
 import { makeFileUploadController, makeFileLoadController } from '@/main/factories/controllers'
+import { makeAuthMiddleware } from '@/main/factories/middlewares'
 
 import { Router } from 'express'
 
 export default (router: Router): void => {
-  router.post('/upload', multerUpload, expressRouterAdapter(makeFileUploadController()))
-  router.get('/load', expressRouterAdapter(makeFileLoadController()))
+  router.post('/upload', expressMiddlewareAdapter(makeAuthMiddleware()), multerUpload, expressRouterAdapter(makeFileUploadController()))
+  router.get('/load', expressMiddlewareAdapter(makeAuthMiddleware()),expressRouterAdapter(makeFileLoadController()))
 }
